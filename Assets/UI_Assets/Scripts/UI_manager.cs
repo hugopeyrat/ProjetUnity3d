6d7d3b777079fa13937108ade708508ba0645cff
DESKTOP-PPPVCDI\Luka <darramon.luka@gmail.com>
2018-03-20 13:55:33 +0100

    UI restant

---
+++ Assets/UI_Assets/Scripts/UI_manager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class UI_manager : MonoBehaviour
+{
+
+    public string PlayScene;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Ouvrir le Menu pause avec getkey
+    }
+
+    public void OnClickplay()
+    {
+        SceneManager.LoadScene(PlayScene);
+    }
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+    public void OnClickUnPaused()
+    {
+        GameObject.Find("MainMenu").SetActive(false);
+    }
+    public void OnClickPaused()
+    {
+        GameObject.Find("MainMenu").SetActive(true);
+
+    }
+
+}
+
+
+
