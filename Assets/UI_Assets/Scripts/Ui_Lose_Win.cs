8b97c766909deece18209c36a2ca25a893ec9871
DESKTOP-PPPVCDI\Luka <darramon.luka@gmail.com>
2018-03-21 16:21:09 +0100

    ui win lose

---
+++ Assets/UI_Assets/Scripts/Ui_Lose_Win.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Ui_Lose_Win : MonoBehaviour
+{
+
+    public string PlayGame;
+    public string PlayMenu;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Ouvrir le Menu pause avec getkey
+    }
+
+    public void OnClickplay()
+    {
+        SceneManager.LoadScene(PlayGame);
+    }
+    public void OnClickExit()
+    {
+        SceneManager.LoadScene(PlayMenu);
+    }
+
+
+}
+
+
+
